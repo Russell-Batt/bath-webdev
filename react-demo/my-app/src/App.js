cd40e2bb6ff6ed18c7f7acbcde11e0d36dc71ba9
Russell-Batt <russellbatt90@gmail.com>
2022-08-02 10:55:21 +0100

    Made an incredibly simple react app

---
+++ react-demo/my-app/src/App.js
@@ -0,0 +1,15 @@
+import Component from './components/component';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>React App</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores error quidem quas fuga blanditiis veniam enim aspernatur voluptates molestiae iste ab vero eligendi reiciendis accusamus voluptatem, magnam nihil. Sed, voluptate?</p>
+      </header>
+      <Component />
+    </div>
+  );
+}
+
+export default App;
---
+++ react-demo/my-app/src/components/component.js
@@ -0,0 +1,10 @@
+function Component() {
+    return (
+        <div>
+            <h1>React Component</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam officiis illum, fuga, quis libero dignissimos minus eos quo rem ratione accusantium omnis delectus iste, doloribus ducimus. Aut laudantium facilis incidunt.</p>
+        </div>
+    )
+}
+
+export default Component;
